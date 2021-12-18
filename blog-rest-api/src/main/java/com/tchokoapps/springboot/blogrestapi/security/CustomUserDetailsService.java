5be8a157d5a090a43501140e55ab1abde6bc46bd
TchokoApps <tchokoapps@gmail.com>
2021-12-18 19:32:54 +0100

    UserDetailsService Interface Implementation

---
+++ blog-rest-api/src/main/java/com/tchokoapps/springboot/blogrestapi/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.tchokoapps.springboot.blogrestapi.security;
+
+import com.tchokoapps.springboot.blogrestapi.entity.Role;
+import com.tchokoapps.springboot.blogrestapi.entity.User;
+import com.tchokoapps.springboot.blogrestapi.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username or email: " + usernameOrEmail));
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), mapRolesToAuthorities());
+
+    }
+
+    Collection<? extends GrantedAuthority> mapRolesToAuthorities(Set<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
+    }
+}
